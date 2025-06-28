4bf82b82954f8e68ee3f0ecd83d558d70f6c7fdb
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-06-28 16:09:29 +0200

    setting up supabase client

---
+++ src/pages/Index.tsx
@@ -0,0 +1,16 @@
+import { getRankingFromAO20 } from "../ao-api/getRankingFromAO20";
+const Index = () => {
+  return (
+    <div>
+      <button
+        onClick={async () => {
+          console.log(await getRankingFromAO20());
+        }}
+      >
+        Click here to test
+      </button>
+    </div>
+  );
+};
+
+export default Index;
---
+++ src/supabase-api/useSupabase.ts
@@ -0,0 +1,4 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://lfimiqkahvapcsqbeeud.supabase.co";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
