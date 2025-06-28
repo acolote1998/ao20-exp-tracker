8f7f5ffd3b96849115d67cda0e03d0475a31ad54
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-06-28 16:45:41 +0200

    functions to get characters from yesterday and today from the DB

--- src/supabase-api/useSupabase.ts
+++ src/supabase-api/useSupabase.ts
@@ -3,11 +3,34 @@ const supabaseUrl = "https://lfimiqkahvapcsqbeeud.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export const testGetData = async () => {
-  const { data, error } = await supabase.from("characters").select("*");
+export const getCharDataFromDbFromYesterday = async () => {
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+  const yesterdayDate = yesterday.toISOString().split("T")[0];
+
+  const { data, error } = await supabase
+    .from("characters")
+    .select("*")
+    .eq("created_at", yesterdayDate);
   if (error) {
     console.error("Error fetching characters:", error);
-    return null; // or throw error
+    return null;
+  }
+  return data;
+};
+
+export const getCharDataFromDbFromToday = async () => {
+  const today = new Date();
+  const todayDate = today.toISOString().split("T")[0];
+
+  const { data, error } = await supabase
+    .from("characters")
+    .select("*")
+    .eq("created_at", todayDate);
+  if (error) {
+    console.error("Error fetching characters:", error);
+    return null;
   }
   return data;
 };
