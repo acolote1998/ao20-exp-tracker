aea19bde7c411c3c4817c727784b850a13c16cb9
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-06-29 10:24:49 +0200

    adding daterange showing

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,6 +5,23 @@ import { getFinalDiffs } from "../logic/getFinalDiffs";
 
 const Index = () => {
   const [charDiffs, setCharDiffs] = useState<CharacterDiff[] | null>(null);
+  const today = new Date();
+
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+
+  const beforeYesterday = new Date(today);
+  beforeYesterday.setDate(today.getDate() - 2);
+
+  const formatDate = (date: Date) => {
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    return `${day}/${month}`;
+  };
+
+  const yesterdayDate = formatDate(yesterday);
+  const beforeYesterdayDate = formatDate(beforeYesterday);
+
   useEffect(() => {
     const fetchDiffs = async () => {
       const diffs = await getFinalDiffs();
@@ -16,16 +33,24 @@ const Index = () => {
 
   return (
     <>
-      {charDiffs !== null && (
-        <div className="grid lg:grid-cols-4 sm:grid-cols-3 gap-2">
-          {charDiffs.map(
-            (char) =>
-              char.exp > 0 && (
-                <CharShowingDiffs key={char.character_name} {...char} />
-              )
-          )}
-        </div>
-      )}
+      <div>
+        <h1 className="text-red-600 text-5xl font-extrabold text-center m-10">
+          OBSCURE
+        </h1>
+        <h3 className="text-white text-3xl font-semibold text-center m-5">
+          {beforeYesterdayDate} - {yesterdayDate}
+        </h3>
+        {charDiffs !== null && (
+          <div className="grid lg:grid-cols-4 sm:grid-cols-3 gap-2">
+            {charDiffs.map(
+              (char) =>
+                char.exp > 0 && (
+                  <CharShowingDiffs key={char.character_name} {...char} />
+                )
+            )}
+          </div>
+        )}
+      </div>
     </>
   );
 };
