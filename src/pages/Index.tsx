0c4a217a500abcb02c321695f07ca3c35515e8ac
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-07-04 18:04:22 +0200

    only rendering characters that are not external

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -20,18 +20,19 @@ const Index = () => {
   return (
     <>
       <div>
-        <h1 className="text-red-600 text-5xl font-extrabold text-center m-10">
-          OBSCURE
-        </h1>
         <h3 className="text-white text-3xl font-semibold text-center m-5">
           AYER
         </h3>
+        <h1 className="text-red-600 text-5xl font-extrabold text-center m-10">
+          OBSCURE
+        </h1>
 
         {charDiffs !== null && (
           <div className="grid xl:grid-cols-4 md:grid-cols-2 gap-2">
             {charDiffs.map(
               (char) =>
-                (char.exp > 0 || char.levelDiff > 0) && (
+                (char.exp > 0 || char.levelDiff > 0) &&
+                !char.external && (
                   <CharShowingDiffs key={char.character_name} {...char} />
                 )
             )}
